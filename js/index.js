3269a8738a2c9ab9f7b723ad9de18489fa1e9e2c
Vladmodzer <terechovvladimir@gmail.com>
2024-03-06 12:53:15 +0100

    add photo + toggle over photo change

--- js/index.js
+++ js/index.js
@@ -33,7 +33,6 @@ apply.addEventListener("click", () => {
   changeColor("--text-col", secondaryColor);
 });
 
-
 const fontFamilySelect = document.getElementById("font-family");
 const customFontFamilyInput = document.getElementById("custom-font-family");
 const btnBg = document.querySelector(".btn-bg ");
@@ -41,13 +40,11 @@ const colorText = document.querySelector(".color-input-text");
 const fontFamilyText = document.querySelector(".font-family");
 fontFamilySelect.addEventListener("change", () => {
   const selectedFont = fontFamilySelect.value;
-//   console.log(selectedFont);
+  //   console.log(selectedFont);
   document.body.style.fontFamily = selectedFont;
   btnBg.style.fontFamily = "Times New Roman";
   colorText.style.fontFamily = "Times New Roman";
   fontFamilyText.style.fontFamily = "Times New Roman";
-  
-
 });
 
 // customFontFamilyInput.addEventListener("input", () => {
@@ -55,3 +52,89 @@ fontFamilySelect.addEventListener("change", () => {
 
 //   // Применить пользовательский шрифт к элементам на странице
 // });
+
+// img upload
+document
+  .getElementById("uploadFileInput")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const imgElement = document.getElementById("uploadedImage");
+      imgElement.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  });
+document
+  .getElementById("uploadFileInput0")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const imgElement = document.getElementById("uploadedImage0");
+      imgElement.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  });
+document
+  .getElementById("uploadFileInput1")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const imgElement = document.getElementById("uploadedImage1");
+      imgElement.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  });
+document
+  .getElementById("uploadFileInput2")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const imgElement = document.getElementById("uploadedImage2");
+      imgElement.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  });
+document
+  .getElementById("uploadFileInput3")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const imgElement = document.getElementById("uploadedImage3");
+      imgElement.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  });
+
+//   drop dawn menu tiggle button
+const toggleCheckbox = document.getElementById("toggle");
+toggleCheckbox.addEventListener("change", handleToggleChange);
+const overPhotoText = document.querySelectorAll(".photo-box__text");
+function handleToggleChange() {
+  // Дейтствие при переключении тумблера
+  const isChecked = this.checked; // Получаем состояние тумблера
+
+  if (isChecked) {
+    overPhotoText.forEach(text => {
+        text.style.color = "white";
+    })
+  } else {
+    overPhotoText.forEach(text => {
+        text.style.color = "black";
+    })
+  }
+}
